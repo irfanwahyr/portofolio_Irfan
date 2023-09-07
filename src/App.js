a593ba72a3c21187d38a4bc54148b2ae05d87c0e
irfan <iramadhani679@gmail.com>
2023-09-07 12:37:41 +0700

    add image

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import './App.css';
-import React, {useState} from 'react';
-import Splash from './LandingPage/splash_screen';
-import LandingPage from './LandingPage/landing_page';
-
 function App() {
-  const [showSplash, setShowSplash] = useState(true);
-  const handleSplashLoaded = () => {
-    setShowSplash(true);
-  };
 
-return(
-  <div className='App'>
-    {showSplash ? (
-        <Splash onLoaded={handleSplashLoaded}/>
-      ) : (
-        <LandingPage/>
-      )
-    }
+  return(
+    <div className='App'>
+
     </div>
   );
 }
